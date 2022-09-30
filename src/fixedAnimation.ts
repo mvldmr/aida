9d8f5d6af8a86d31845938eb8fd82a61ef205c5b
mvldmr <unknwnlifestyle@gmail.com>
2022-09-30 23:17:10 +0400

    update page

--- src/fixedAnimation.ts
+++ src/fixedAnimation.ts
@@ -1,52 +1,57 @@
 export function setupFixedAnimation(wrapperList: HTMLElement[]) {
 	wrapperList.forEach(wrapper => {
+		const distance = getAttributeInNum(wrapper, 'data-distance');
 		const cards = Array.prototype.slice.call(wrapper.querySelectorAll('.fix-anim'));
-		const distance = cards.length ? cards[0].clientWidth : 0;
-		const wrapperHeight = cards.length ? (cards[0].clientHeight + distance) * (cards.length - 1) : 0;
-		wrapper.style.height = wrapperHeight + 'px';
+		const cardHeight = cards.length ? cards[0].clientHeight : 0;
+		wrapper.style.height = distance + cardHeight + 'px';
 		fixCards(wrapper, cards, distance);
 	});
 }
 
+function getAttributeInNum(element:HTMLElement, attributeName:string) {
+	const attribute = element ? element.getAttribute(attributeName) : '';
+	console.log(attribute, attributeName);
+	if (attribute) return parseInt(attribute, 10);
+	return 0;
+}
+
 function fixCards(wrapper:HTMLElement, cards:HTMLDivElement[], distance: number) {
-	wrapper.style.borderBottom = '5px solid red';
-	let wrapperTopPos = wrapper.getBoundingClientRect().top + window.scrollY;
-	const wrapperBottomPos = wrapper.getBoundingClientRect().bottom + window.scrollY;
+	const topOffset = getAttributeInNum(wrapper, 'data-top-offset');
+	let startPosition = wrapper.getBoundingClientRect().top + window.scrollY;
+	startPosition += topOffset;
+	const endPosition = startPosition + distance;
 	const windowHeight = window.innerHeight;
-	const topOffset = 50;
-	wrapperTopPos -= topOffset;
+	const firstCard = wrapper.querySelector('.fix-anim');
+	const cardDistance = firstCard ? firstCard.clientWidth : 0;
 	window.addEventListener('scroll', () => {
 		cards.forEach((card, i) => {
-			if (window.scrollY >= wrapperTopPos && window.scrollY + windowHeight < wrapperBottomPos) {
+			if (window.scrollY >= startPosition && window.scrollY < endPosition) {
 				if (getComputedStyle(card).position !== 'fixed') {
 					const cachedTransform = getComputedStyle(card).transform;
 					card.style.transform = '';
 					const cachedRect = card.getBoundingClientRect();
-					card.classList.remove('fixed-anim-ended');
 					card.style.transform = cachedTransform;
 					card.style.position = 'fixed';
 					card.style.left = cachedRect.left + 'px';
 					card.style.top = cachedRect.top + 'px';
 					card.style.width = cachedRect.width + 'px';
 				}
-				if (window.scrollY - wrapperTopPos < distance) {
-					if (i === 1 || i === 2) card.style.transform = `translateX(${window.scrollY - wrapperTopPos}px)`;
-				} else if (window.scrollY - wrapperTopPos < distance * 2) {
-					if (i === 1) card.style.transform = `translateX(${distance}px)`;
-					if (i === 2) card.style.transform = `translateX(${window.scrollY - wrapperTopPos}px)`;
-				} else if (window.scrollY - wrapperTopPos < distance * 3) {
-					if (i === 2) card.style.transform = `translateX(${distance * 2}px)`;
+				if (window.scrollY - startPosition < cardDistance) {
+					if (i === 1 || i === 2) card.style.transform = `translateX(${window.scrollY - startPosition}px)`;
+				} else if (window.scrollY - startPosition < cardDistance * 2) {
+					if (i === 1) card.style.transform = `translateX(${cardDistance}px)`;
+					if (i === 2) card.style.transform = `translateX(${window.scrollY - startPosition}px)`;
+				} else if (window.scrollY - startPosition < cardDistance * 3) {
+					if (i === 2) card.style.transform = `translateX(${cardDistance * 2}px)`;
 				}
 
-			} else if (window.scrollY + windowHeight > wrapperBottomPos) {
+			} else if (window.scrollY + windowHeight >= endPosition) {
 				if (getComputedStyle(card).position === 'fixed') {
-					card.classList.add('fixed-anim-ended');
-					if (i === 1) card.style.transform = `translateX(${distance}px)`;
-					if (i === 2) card.style.transform = `translateX(${distance * 2}px)`;
-					console.log(wrapper.clientHeight, card.clientHeight);
+					if (i === 1) card.style.transform = `translateX(${cardDistance}px)`;
+					if (i === 2) card.style.transform = `translateX(${cardDistance * 2}px)`;
 					card.style.position = '';
 					card.style.left = '';
-					card.style.top = wrapper.clientHeight - card.clientHeight + 'px';
+					card.style.top = distance + 'px';
 					card.style.width = '';
 				}
 			} else {

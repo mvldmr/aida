15d8507bc3096f2ce9cbfc23bdc5436e4426b7f3
mvldmr <unknwnlifestyle@gmail.com>
2022-10-01 15:03:59 +0400

    update project

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import './style.scss'
 import { setupAnimation } from './setAnimation'
 import {setupFixedAnimation} from "./fixedAnimation";
 
-setupAnimation(Array.prototype.slice.call(document.querySelectorAll('.anim')));
-setupFixedAnimation(Array.prototype.slice.call(document.querySelectorAll('.fix-anim-wrapper')));
+onPageLoaded(() => {
+	setupAnimation(Array.prototype.slice.call(document.querySelectorAll('.anim')));
+	setupFixedAnimation(Array.prototype.slice.call(document.querySelectorAll('.fix-anim-wrapper')));
+})
+
+function onPageLoaded(cb:Function) {
+	document.readyState === 'complete' ? cb() : window.addEventListener('load', function() {cb()});
+}

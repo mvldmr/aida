bd47aec574210031ef9fd881f6ddb124cb491c09
mvldmr <unknwnlifestyle@gmail.com>
2022-10-01 12:56:45 +0400

    update page

--- src/fixedAnimation.ts
+++ src/fixedAnimation.ts
@@ -1,29 +1,45 @@
 export function setupFixedAnimation(wrapperList: HTMLElement[]) {
+	const changeEvent = 'ontouchend' in document ? 'orientationchange' : 'resize';
+	let winWidth = document.documentElement.clientWidth;
 	wrapperList.forEach(wrapper => {
 		const distance = getAttributeInNum(wrapper, 'data-distance');
 		const cards = Array.prototype.slice.call(wrapper.querySelectorAll('.fix-anim'));
 		const cardHeight = cards.length ? cards[0].clientHeight : 0;
-		wrapper.style.height = distance + cardHeight + 'px';
-		fixCards(wrapper, cards, distance);
+		if (winWidth > 768) {
+			wrapper.style.height = distance + cardHeight + 'px';
+			wrapper.setAttribute('animation-active', 'y');
+			fixCards(wrapper, cards, distance);
+		}
+		window.addEventListener(changeEvent, function() {
+			setTimeout(function() {
+				winWidth = document.documentElement.clientWidth;
+				const isAnimActive = wrapper.getAttribute('animation-active') === 'y';
+				if ((isAnimActive && winWidth > 768) || (!isAnimActive && winWidth < 768)) return;
+				if (winWidth > 768) {
+					wrapper.setAttribute('animation-active', 'y');
+					wrapper.style.height = distance + cardHeight + 'px';
+					fixCards(wrapper, cards, distance);
+				} else {
+					wrapper.removeAttribute('animation-active');
+					wrapper.style.height = '';
+					fixCards(wrapper, cards, distance);
+				}
+			}, 500);
+		})
 	});
 }
 
-function getAttributeInNum(element:HTMLElement, attributeName:string) {
-	const attribute = element ? element.getAttribute(attributeName) : '';
-	console.log(attribute, attributeName);
-	if (attribute) return parseInt(attribute, 10);
-	return 0;
-}
-
 function fixCards(wrapper:HTMLElement, cards:HTMLDivElement[], distance: number) {
+	const changeEvent = 'ontouchend' in document ? 'orientationchange' : 'resize';
+	const isActiveAnim = wrapper.getAttribute('animation-active') === 'y';
 	const topOffset = getAttributeInNum(wrapper, 'data-top-offset');
 	let startPosition = wrapper.getBoundingClientRect().top + window.scrollY;
 	startPosition += topOffset;
-	const endPosition = startPosition + distance;
+	let endPosition = startPosition + distance;
 	const windowHeight = window.innerHeight;
-	const firstCard = wrapper.querySelector('.fix-anim');
-	const cardDistance = firstCard ? firstCard.clientWidth : 0;
-	window.addEventListener('scroll', () => {
+	let cardWidth = cards[0] ? cards[0].clientWidth : 0;
+
+	const updateCard = () => {
 		cards.forEach((card, i) => {
 			if (window.scrollY >= startPosition && window.scrollY < endPosition) {
 				if (getComputedStyle(card).position !== 'fixed') {
@@ -36,19 +52,19 @@ function fixCards(wrapper:HTMLElement, cards:HTMLDivElement[], distance: number)
 					card.style.top = cachedRect.top + 'px';
 					card.style.width = cachedRect.width + 'px';
 				}
-				if (window.scrollY - startPosition < cardDistance) {
+				if (window.scrollY - startPosition < cardWidth) {
 					if (i === 1 || i === 2) card.style.transform = `translateX(${window.scrollY - startPosition}px)`;
-				} else if (window.scrollY - startPosition < cardDistance * 2) {
-					if (i === 1) card.style.transform = `translateX(${cardDistance}px)`;
+				} else if (window.scrollY - startPosition < cardWidth * 2) {
+					if (i === 1) card.style.transform = `translateX(${cardWidth}px)`;
 					if (i === 2) card.style.transform = `translateX(${window.scrollY - startPosition}px)`;
-				} else if (window.scrollY - startPosition < cardDistance * 3) {
-					if (i === 2) card.style.transform = `translateX(${cardDistance * 2}px)`;
+				} else if (window.scrollY - startPosition < cardWidth * 3) {
+					if (i === 2) card.style.transform = `translateX(${cardWidth * 2}px)`;
 				}
 
 			} else if (window.scrollY + windowHeight >= endPosition) {
 				if (getComputedStyle(card).position === 'fixed') {
-					if (i === 1) card.style.transform = `translateX(${cardDistance}px)`;
-					if (i === 2) card.style.transform = `translateX(${cardDistance * 2}px)`;
+					if (i === 1) card.style.transform = `translateX(${cardWidth}px)`;
+					if (i === 2) card.style.transform = `translateX(${cardWidth * 2}px)`;
 					card.style.position = '';
 					card.style.left = '';
 					card.style.top = distance + 'px';
@@ -61,7 +77,34 @@ function fixCards(wrapper:HTMLElement, cards:HTMLDivElement[], distance: number)
 				card.style.width = '';
 				card.style.transform = '';
 			}
-
 		});
-	});
+	}
+
+	const updateCardDistance = () => {
+		setTimeout(function() {
+			if (document.documentElement.clientWidth < 768) {
+				window.removeEventListener('scroll', updateCard);
+				return;
+			}
+			cardWidth = cards[0] ? cards[0].clientWidth : 0;
+			startPosition = wrapper.getBoundingClientRect().top + window.scrollY;
+			startPosition += topOffset;
+			endPosition = startPosition + distance;
+		}, 500);
+	}
+
+	if (!isActiveAnim) {
+		window.removeEventListener('scroll', updateCard);
+		return;
+	}
+	window.removeEventListener('scroll', updateCard);
+	window.addEventListener('scroll', updateCard);
+	window.removeEventListener(changeEvent, updateCardDistance);
+	window.addEventListener(changeEvent, updateCardDistance);
+}
+
+function getAttributeInNum(element:HTMLElement, attributeName:string) {
+	const attribute = element ? element.getAttribute(attributeName) : '';
+	if (attribute) return parseInt(attribute, 10);
+	return 0;
 }

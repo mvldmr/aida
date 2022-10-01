c4841d4e913a340e04a93bd9ea5a7253d50c1c0c
mvldmr <unknwnlifestyle@gmail.com>
2022-10-01 13:15:55 +0400

    create page

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import './style.scss'
-import { setupAnimation } from './setAnimation'
-import {setupFixedAnimation} from "./fixedAnimation";
-
-setupAnimation(Array.prototype.slice.call(document.querySelectorAll('.anim')));
-setupFixedAnimation(Array.prototype.slice.call(document.querySelectorAll('.fix-anim-wrapper')));

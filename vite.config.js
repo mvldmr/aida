d929a5792a0ecadb1312e608e5a04b1c1a704001
mvldmr <unknwnlifestyle@gmail.com>
2022-11-24 17:06:11 +0400

    add eng version

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-	base: 'aida/'
+	base: 'aida/',
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				eng: resolve(__dirname, 'eng.html')
+			}
+		}
+	}
 })
